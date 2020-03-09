33f476dc09fcca0c61216f6043c62ff835f96436
selimmizrahi <mizrahiselim@gmail.com>
2020-03-09 13:00:28 +0200

    this is my 5th milestone

--- javascript_exercise_1/selim_script.js
+++ javascript_exercise_1/selim_script.js
@@ -1,17 +1,10 @@
-function recurse(n){
-    if (n==0) {
-        return 0;
-    }
-    else if(n==1){
-        return 1;
-    }
-    else if(n>1){
-       let result = recurse(n-2) + recurse(n-1)
-       return result
-    }
-}
-
 function myFunction() {
     let input = document.getElementById('input').value;
-    document.getElementById('output').innerHTML = recurse(input) ;
-  }
+    fetch(`http://localhost:5050/fibonacci/${input}`)
+        .then(res => res.json()) 
+        .then(function(data) {
+            let y = data.result; 
+            document.getElementById('output').innerHTML = y;
+            console.log(y);
+        });
+}
